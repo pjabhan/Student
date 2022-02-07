2a3aa1d72f74ba18e6ce31c441088cc992b97a77
pjabhan <pbhandari1019@gmail.com>
2022-02-07 15:24:48 -0600

    First Commit

---
+++ stdnt_project/src/main/java/com/stdnt_mgmt/StdntProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stdnt_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdntProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdntProjectApplication.class, args);
+	}
+
+}
---
+++ stdnt_project/src/test/java/com/stdnt_mgmt/StdntProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stdnt_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdntProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
